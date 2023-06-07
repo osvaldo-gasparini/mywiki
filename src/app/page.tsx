9b8fe6cc99f9ed7021357c5599f36c7fdc6cb0d8
osvaldo-gasparini <osvaldo@selehann.com>
2023-06-07 20:40:47 -0300

    dashboard ui base complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,18 @@ import TextArea from "../../components/UI/Inputs/TextArea/TextArea";
 import style from "./page.module.css";
 
 const Dashboard = () => {
+  const WIKIS = [
+    {
+      label: "Personal Development",
+    },
+    {
+      label: "Web Development",
+    },
+    {
+      label: "Physical Development",
+    },
+  ];
+
   return (
     <div className={style.pageContainer}>
       <Header />
@@ -16,13 +28,41 @@ const Dashboard = () => {
           <article className={`${style.create} ${style.article}`}>
             <h2>I&apos;m gonna create</h2>
             <div className={style.buttonContainer}>
-              <span>Wiki</span>
-              <Button className={style.noteButton}>Note</Button>
+              <Button className={`${style.button} ${style.wikiButton}`}>
+                Wiki
+              </Button>
+              <Button className={`${style.button} ${style.noteButton}`}>
+                Note
+              </Button>
             </div>
           </article>
-          <article className={`${style.create} ${style.article}`}>
-            <h2>I&apos;m gonna create</h2>
-            <button></button>
+          <article className={`${style.settings} ${style.article}`}>
+            <Button>Wikis</Button>
+            <Button>Notes</Button>
+            <Button>Trash</Button>
+            <Button>Settings</Button>
+          </article>
+        </section>
+        <section className={style.secondSection}>
+          <article className={style.container}>
+            <h3 className={style.title}>Wiki&apos;s</h3>
+            <div className={`${style.itemContainer} ${style.wikis}`}>
+              {WIKIS.map((item, index) => {
+                return (
+                  <Button key={`${item.label}${index}`}>{item.label}</Button>
+                );
+              })}
+            </div>
+          </article>
+          <article className={style.container}>
+            <h3 className={style.title}>Note&apos;s</h3>
+            <div className={`${style.itemContainer} ${style.notes}`}>
+              {WIKIS.map((item, index) => {
+                return (
+                  <Button key={`${item.label}${index}`}>{item.label}</Button>
+                );
+              })}
+            </div>
           </article>
         </section>
       </main>
